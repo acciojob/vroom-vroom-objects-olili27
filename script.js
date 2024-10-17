e251e6b7f2bc290674e9e8946bfe94e1a02cc301
Timothy Olili <timmolili27@gmail.com>
2024-10-18 01:06:16 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = funtion() {
+	return `${this.make} ${this.model}`;
+}
+
+function SportsCar(make, model, topSpeed) {
+	this.topSpeed = topSpeed;
+}
 
 // Do not change the code below
 window.Car = Car;
